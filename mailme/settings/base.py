67f62f2a73de532edd5c8e80cba62a33ca646281
Christopher Grebs <cg@webshox.org>
2013-12-12 23:38:31 -0800

    base db.sqlite3 in project root, gitignore it

--- mailme/settings/base.py
+++ mailme/settings/base.py
@@ -44,7 +44,7 @@ WSGI_APPLICATION = 'mailme.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(PROJECT_DIR, 'db.sqlite3'),
     }
 }
 
