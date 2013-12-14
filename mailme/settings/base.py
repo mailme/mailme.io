f777c272ff059d05f450c87ff6f0c1abb1aa73e7
Christopher Grebs <cg@webshox.org>
2013-12-14 04:47:26 -0800

    Add media config, hide collected_static folder, it's not needed in development either way and in prod we want a CDN

--- mailme/settings/base.py
+++ mailme/settings/base.py
@@ -75,7 +75,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+STATIC_ROOT = os.path.join(BASE_DIR, '.collected_static')
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # 3 hours
 MAILME_REFRESH_EVERY = 3 * 60 * 60
