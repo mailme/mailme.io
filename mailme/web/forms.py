a15eead6c7144622bdac59972d0a3349581b277d
Christopher Grebs <cg@webshox.org>
2013-12-22 07:40:06 -0800

    Add csrf validation, render form errors properly

--- mailme/web/forms.py
+++ mailme/web/forms.py
@@ -1,8 +1,25 @@
 from django import forms
+from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext_lazy, ugettext as _
+
+from mailme.core.models import User
 
 
 class RegisterForm(forms.Form):
     email = forms.EmailField(
-        label='Your email address',
+        label=ugettext_lazy('Your email address'),
         widget=forms.TextInput({'placeholder': 'support@mailme.io'})
     )
+
+    def clean_email(self):
+        """
+        Validates if the required field `email` contains
+        a non existing mail address.
+        """
+        exists = User.objects.filter(email=self.cleaned_data['email']).exists()
+        if exists:
+            raise forms.ValidationError(mark_safe(
+                _('The given email address is already in use. '
+                  '<a href="{link}">Forgot your password?</a>'.format(link=''))
+            ))
+        return self.cleaned_data['email']
--- mailme/web/views.py
+++ mailme/web/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from mailme.web.forms import RegisterForm
+from mailme.core.models import User
 
 
 def index(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            # register
-            pass
+            print("valid")
     else:
         form = RegisterForm()
     return render(request, 'mailme/web/index.html', {
